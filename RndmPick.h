aaee3ea153ece0c7af2e651e799222978455c704
elisalazzaroni542 <124351459+elisalazzaroni542@users.noreply.github.com>
2023-02-03 12:57:25 +0100

    Create RndmPick.h

---
+++ RndmPick.h
@@ -0,0 +1,10 @@
+#ifndef RANDOMPICK_H
+#define RANDOMPICK_H
+
+#include "TRandom3.h"
+
+class RndmPick : public TRandom3{
+  //classe che avrà il compito di generare tutti i numeri casuali.
+  public:
+  RndmPick();
+  RndmPick(
