81268330703812675476774826bc1fee9305b41a
Davide CristoforettiÂ <dcristof9@gmail.com>
2024-12-22 16:18:45 +0100

    improved compile script

--- project/bash/compile.sh
+++ project/bash/compile.sh
@@ -5,13 +5,34 @@ ABS_PATH=$(pwd)
 SOURCE_DIR="$ABS_PATH/src"
 HEADER_DIR="$ABS_PATH/headers"
 
-export LD_LIBRARY_PATH="$SOURCE_DIR:$LD_LIBRARY_PATH"
+#export LD_LIBRARY_PATH="$SOURCE_DIR:$LD_LIBRARY_PATH"
+
 
 cat > rootlogon.C << EOF
 {
     gSystem->AddIncludePath("-I$HEADER_DIR");
+
+    if (!gSystem->AccessPathName("$SOURCE_DIR/Event_cxx.so")) {
+        gSystem->Load("$SOURCE_DIR/Event_cxx.so");
+    }
+
+
+    if (!gSystem->AccessPathName("$SOURCE_DIR/Trajectory_cxx.so")) {
+        gSystem->Load("$SOURCE_DIR/Trajectory_cxx.so");
+    }
+    
+    if (!gSystem->AccessPathName("$SOURCE_DIR/HitPoint_cxx.so")) {
+        gSystem->Load("$SOURCE_DIR/HitPoint_cxx.so");
+    }
 }
 EOF
+
+
+#cat > rootlogon.C << EOF
+#{
+#    gSystem->AddIncludePath("-I$HEADER_DIR");
+#}
+#EOF
 
 root -b -l -q "$SOURCE_DIR/Event.cxx+" || true
 root -b -l -q "$SOURCE_DIR/Trajectory.cxx+" || true
